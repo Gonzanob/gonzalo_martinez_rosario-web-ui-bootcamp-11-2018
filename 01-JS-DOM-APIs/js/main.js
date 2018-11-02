f439f3d1955a30dda62edd968e4f843ade72faa3
Gonzanob <gonzanob99@gmail.com>
2018-11-02 11:21:42 -0300

    Added excercise 1

---
+++ 01-JS-DOM-APIs/js/main.js
@@ -0,0 +1,8 @@
+document.onload(unvanish());
+
+ function unvanish() {
+     let tounvanish = document.getElementById("toshowsection");
+     tounvanish.style.display = "block";
+     tounvanish.style.animation = "fadeIn 2s";
+//     tounvanish.setAttribute("style", "visibility: visible; opacity: 1;");
+ }
