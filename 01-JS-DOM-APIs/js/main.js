d081f4dcce097c49ecf822eb61de4e884ce30891
Gonzanob <gonzanob99@gmail.com>
2018-11-04 16:54:34 -0300

    Added excercise 3

--- 01-JS-DOM-APIs/js/main.js
+++ 01-JS-DOM-APIs/js/main.js
@@ -11,4 +11,40 @@ function showAlert() {
     alert("Want some magic?");
 }
 
-document.getElementById("alertbutton").addEventListener("click", showAlert);
+function requestJoke()
+{
+    var promise = requestApi("GET", "http://api.icndb.com/jokes/random", true);
+    promise.then(function(obj)
+    {
+        var jokejson = JSON.parse(obj);
+        document.getElementById("jokeplace").textContent = jokejson.value["joke"];    
+    }, function(response)
+    {
+        console.log(response);
+        document.getElementById("toshowsection").style.backgroundColor = "red";
+    });
+}
+
+function requestApi(reqtype, url, sync)
+{
+    
+    var promise = new Promise(function(resolve,reject) 
+    {
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function()
+        {
+            if(this.readyState == 4 && this.status == 200)
+            {
+                resolve(this.response);
+            }
+
+        }
+        xhttp.open(reqtype, url, sync);
+        xhttp.send();
+        
+    });
+
+    return promise;
+}
+
+document.getElementById("alertbutton").addEventListener("click", requestJoke);
