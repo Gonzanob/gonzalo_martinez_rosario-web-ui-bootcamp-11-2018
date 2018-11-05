558ccc8325bd850ae648443bfc9e9d625a01043c
Gonzanob <gonzanob99@gmail.com>
2018-11-04 23:00:03 -0300

    Added final excercise and removed some unused things in the previous excercise

--- 01-JS-DOM-APIs/js/main.js
+++ 01-JS-DOM-APIs/js/main.js
@@ -50,6 +50,7 @@ function requestApi(reqtype, url, sync)
 document.getElementById("alertbutton").addEventListener("click", requestJoke);
 var qbutton = document.getElementById("qbutton");
 qbutton.addEventListener("click", requestviagit);
+document.getElementById("qbutton_table").addEventListener("click", requestviagit_table);
 
 function requestviagit()
 {
@@ -65,7 +66,6 @@ function requestviagit()
         var cont = document.getElementById("repo-container");
         var fragment = document.createDocumentFragment();
         var list = document.createElement("ul");
-        var k = items.length;
         for( i = 0 ; i < items.length; i++) 
             {
                 var templist = document.createElement("li");
@@ -75,5 +75,60 @@ function requestviagit()
             }
         fragment.appendChild(list);
         cont.appendChild(fragment);
+    });
+}
+
+function requestviagit_table()
+{
+    var tosearch = document.getElementById("tosearch")
+    var textosearch = tosearch.value;
+    var promise = requestApi("GET", "https://api.github.com/search/repositories?q=" + "'" + textosearch + "'", true);
+    promise.then(function (obj)
+    {
+        var obj_json = JSON.parse(obj);
+        return obj_json.items;
+    }).then(function(items)
+    {
+        var fragment = document.createDocumentFragment();
+        var table = document.createElement("table")
+        table.style.width = "100%";
+        var header1, header2, header3, headercontainer, htext1, htext2, htext3;
+        headercontainer = document.createElement("tr");
+        header1 = document.createElement("th");
+        header2 = document.createElement("th");
+        header3 = document.createElement("th");
+        htext1 = document.createTextNode("Name");
+        htext2 = document.createTextNode("Full Name");
+        htext3 = document.createTextNode("URL");
+        header1.appendChild(htext1);
+        header2.appendChild(htext2);
+        header3.appendChild(htext3);
+        headercontainer.appendChild(header1);
+        headercontainer.appendChild(header2);
+        headercontainer.appendChild(header3);
+        table.appendChild(headercontainer);
+
+        for( i = 0 ; i < items.length; i++) 
+            {
+                var temp1, temp2, temp3, temptext1, temptext2, temptext3, temprow;
+                temprow = document.createElement("tr");
+                temp1 = document.createElement("td");
+                temp2 = document.createElement("td");
+                temp3 = document.createElement("td");
+                temptext1 = document.createTextNode(items[i].name);
+                temptext2 = document.createTextNode(items[i].full_name);
+                temptext3 = document.createTextNode(items[i].url);
+                temp1.appendChild(temptext1);
+                temp2.appendChild(temptext2);
+                temp3.appendChild(temptext3);
+                temprow.appendChild(temp1);
+                temprow.appendChild(temp2);
+                temprow.appendChild(temp3);
+                table.appendChild(temprow);
+            }
+        console.log(table);
+        fragment.appendChild(table);
+        document.body.appendChild(fragment);
+        
     });
 }
